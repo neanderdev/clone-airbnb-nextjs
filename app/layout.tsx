b4571f672b58bf1f3253f2120e1b6ce14009202d
Neander de Souza <neander.faria@hotmail.com>
2024-08-12 10:44:55 -0300

    Environment setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+
+import "./globals.css";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>
+        {children}
+      </body>
+    </html>
+  );
+}

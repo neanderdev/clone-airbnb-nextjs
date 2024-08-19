f29cb07275a019133441972cafe8a794ae58f946
Neander de Souza <neander.faria@hotmail.com>
2024-08-19 10:46:42 -0300

    Listing creation step 5 (Description and Price, Listing creation POST route)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import axios from "axios";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 import { useMemo, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 
 import { useRentModal } from "../../hooks/useRentModal";
 
@@ -11,6 +14,7 @@ import { CategoryInput } from "../inputs/CategoryInput";
 import { Counter } from "../inputs/Counter";
 import { CountrySelect } from "../inputs/CountrySelect";
 import { ImageUpload } from "../inputs/ImageUpload";
+import { Input } from "../inputs/Input";
 import { categories } from "../navbar/Categories";
 import { Modal } from "./Modal";
 
@@ -24,9 +28,11 @@ enum STEPS {
 };
 
 export function RentModal() {
+    const router = useRouter();
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+    const [isLoading, setIsLoading] = useState(false);
 
     const {
         register,
@@ -77,6 +83,33 @@ export function RentModal() {
     function onNext() {
         setStep((value) => value + 1);
     }
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        if (step !== STEPS.PRICE) {
+            return onNext();
+        }
+
+        setIsLoading(true);
+
+        axios.post("/api/listings", data)
+            .then(() => {
+                toast.success("Listing Created!");
+
+                router.refresh();
+
+                reset();
+
+                setStep(STEPS.CATEGORY);
+
+                rentModal.onClose();
+            })
+            .catch(() => {
+                toast.success("Something went wrong.");
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
 
     const actionLabel = useMemo(() => {
         if (step === STEPS.PRICE) {
@@ -188,11 +221,64 @@ export function RentModal() {
         );
     }
 
+    if (step === STEPS.DESCRIPTION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="How would you description your place?"
+                    subtitle="Short and sweet works best!"
+                />
+
+                <Input
+                    id="title"
+                    label="Title"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+
+                <hr />
+
+                <Input
+                    id="description"
+                    label="Description"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        );
+    }
+
+    if (step === STEPS.PRICE) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Now, set your price"
+                    subtitle="How much do you charge per night?"
+                />
+
+                <Input
+                    id="price"
+                    label="Price"
+                    formatPrice
+                    type="number"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        );
+    }
+
     return (
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={onNext}
+            onSubmit={handleSubmit(onSubmit)}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
